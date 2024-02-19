15aac0ff83d7167954b393fd8b6babc4d532a0a5
Reem-degais <reem.degais.33@gmail.com>
2024-02-19 16:11:29 +0300

    create products model, display the product in home page and store page

--- ecommerce/category/models.py
+++ ecommerce/category/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Catecory(models.Model):
-    catecory_name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(max_length=250, blank=True)
-    catecory_img = models.ImageField(upload_to='photoes/catecgories', blank=True)
+    catecory_img = models.ImageField(upload_to='photos/catecgories', blank=True)
 
     class Meta:
        verbose_name = 'category'
        verbose_name_plural = 'catecgories'
     def __str__(self):
-        return self.catecory_name
+        return self.category_name
