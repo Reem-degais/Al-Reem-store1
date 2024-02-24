314aeacd30afd2d58b6d996aaee178709a5bd675
Reem-degais <reem.degais.33@gmail.com>
2024-02-24 09:25:20 +0300

    create payment, order and product_order models & place_order method

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'orders.apps.OrdersConfig',
     'cart.apps.CartConfig',
     'store.apps.StoreConfig',
     'accounts.apps.AccountsConfig',
